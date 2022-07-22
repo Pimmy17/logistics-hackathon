22312629ed2512be8083bef37783f61203731f1e
virajpatel955 <vpatel97@hotmail.com>
2022-07-22 11:23:22 +0100

    Amendments

--- src/main/java/com/qa/logincontroller/LoginController.java
+++ src/main/java/com/qa/logincontroller/LoginController.java
@@ -1,17 +1,55 @@
 package com.qa.logincontroller;
 
-public class LoginController {
+import java.util.List;
 
-	public class LoginController implements CrudController<UserLogin> {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.LoginDAO.CrudController;
+import com.qa.LoginDAO.UserLoginDAO;
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.logins.UserLogins;
+import com.qa.logisticshackathon.utils.Utils;
+
+public class LoginController implements CrudController<UserLogins> {
 		
 		public static final Logger LOGGER = LogManager.getLogger();
 		
 		private UserLoginDAO userlogindao;
 		private Utils utils;
 		
-		public LoginController(UserLoginDAO )
+		public LoginController(UserLoginDAO userloginDAO, Utils utils ) {
+			super();
+			this.userlogindao = userloginDAO;
+			this.utils = utils;
 		
 	}
+
+		@Override
+		public List<UserLogin> readAll() {
+			List<UserLogins> userlogins = UserLoginDAO.readAll();
+			for (UserLogins userlogin : userlogins) {
+				LOGGER.info(userlogin);
+			return null;
+		}
+
+		@Override
+		public UserLogin create() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public UserLogin update() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public int delete() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
 	
 	
 }
